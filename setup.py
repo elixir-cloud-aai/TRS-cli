7322e7adce32a788687d6eab9ccedf5a062ff435
Alex Kanitz <alexander.kanitz@unibas.ch>
2021-11-09 18:38:47 +0100

    build: support Python 3.9 (#47)

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=[
         'pydantic>=1.6.1',
